5a5a5dcbe6ad3b25ac4ede778cb0df5892f56289
Gawsan <67488752+Gawsan@users.noreply.github.com>
2021-10-08 12:17:13 +0530

    get the five number issue

--- main.cpp
+++ main.cpp
@@ -4,12 +4,19 @@ using namespace std;
 
 int main() {
 	calculator a;
-	int x, y;
+	int x, y ,c[5];
   //getting values from client
 	cout << "Enter starting number : ";
 	cin >>x;
 	cout << "Enter Ending  number :  ";
 	cin >> y;
+
+//checking five numbers
+	// for(int i=0;i<5;i++){
+	// 	cout<<"Get the git id ";
+	// 	cin>>c[i];
+		
+	// }
 
 	a.setValue(x, y);
 	a.display();
